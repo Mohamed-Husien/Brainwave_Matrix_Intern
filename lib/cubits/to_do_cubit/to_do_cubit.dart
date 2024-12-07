e742f2001c2185a743b4d5662144d7136dfde2fe
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2024-12-07 13:46:43 +0200

    create  fetchAllToDo function

--- lib/cubits/to_do_cubit/to_do_cubit.dart
+++ lib/cubits/to_do_cubit/to_do_cubit.dart
@@ -1,8 +1,15 @@
 import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:brainwave_matrix_intern_to_do_app/models/to_do_item_model.dart';
+import 'package:hive_flutter/adapters.dart';
 
 part 'to_do_state.dart';
 
 class ToDoCubit extends Cubit<ToDoState> {
   ToDoCubit() : super(ToDoInitial());
+  List<ToDoItemModel>? toDos;
+  fetchAllToDo() async {
+    var toDoBox = Hive.box<ToDoItemModel>('to_do');
+    toDos = toDoBox.values.toList();
+    emit(ToDoInSuccess());
+  }
 }
