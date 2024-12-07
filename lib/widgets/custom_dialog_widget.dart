ccaf313c3132eb8c60291851c82fe264292f0be7
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2024-12-07 11:57:29 +0200

    make dialog be responsive

--- lib/widgets/custom_dialog_widget.dart
+++ lib/widgets/custom_dialog_widget.dart
@@ -5,11 +5,15 @@ class CustomDialogWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
     return AlertDialog(
+      backgroundColor: Colors.yellow[300],
       content: Container(
-        height: 120,
+        height: screenHeight * 0.3,
+        width: screenWidth * 0.8,
         decoration: BoxDecoration(
-          color: Colors.yellow,
+          color: Colors.yellow[300],
           borderRadius: BorderRadius.circular(12),
         ),
       ),
