f2c0af3b3a53de2346ac55b6e5127c1d4ccc2067
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2024-12-07 11:41:09 +0200

    add addToDo Function

--- lib/cubits/add_to_do_cubit/add_to_do_cubit.dart
+++ lib/cubits/add_to_do_cubit/add_to_do_cubit.dart
@@ -1,8 +1,25 @@
+import 'dart:developer';
+
 import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:brainwave_matrix_intern_to_do_app/models/to_do_item_model.dart';
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 
 part 'add_to_do_state.dart';
 
 class AddToDoCubit extends Cubit<AddToDoState> {
   AddToDoCubit() : super(AddToDoInitial());
+
+  addToDo(ToDoItemModel toDo) async {
+    emit(AddToDoInitial());
+
+    try {
+      var toDoBox = Hive.box<ToDoItemModel>('to_do');
+      await toDoBox.add(toDo);
+      emit(AddToDoSucess());
+    } catch (e) {
+      emit(AddToDoFailure());
+      log(e.toString());
+    }
+  }
 }
