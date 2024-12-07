e839baf5c52c42ec6524f94c056842b5e689f307
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2024-12-07 13:16:16 +0200

    provide add todo  cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:brainwave_matrix_intern_to_do_app/cubits/add_to_do_cubit/add_to_do_cubit.dart';
 import 'package:brainwave_matrix_intern_to_do_app/models/to_do_item_model.dart';
 import 'package:brainwave_matrix_intern_to_do_app/views/home_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 
 void main() async {
@@ -15,16 +17,19 @@ class ToDoApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.yellow,
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.yellow, // Set the AppBar color here
-          elevation: 0, // Optional: Match your AppBar's elevation
+    return BlocProvider(
+      create: (context) => AddToDoCubit(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.yellow,
+          appBarTheme: const AppBarTheme(
+            backgroundColor: Colors.yellow, // Set the AppBar color here
+            elevation: 0, // Optional: Match your AppBar's elevation
+          ),
         ),
+        home: const HomeView(),
       ),
-      home: const HomeView(),
     );
   }
 }
